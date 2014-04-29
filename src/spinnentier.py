aee47c8e90347375a47d7d2d92390430033b7979
Rylan <rylans@gmail.com>
2014-04-29 13:28:11 -0700

    Added logging.

--- src/spinnentier.py
+++ src/spinnentier.py
@@ -1,31 +1,32 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from urlparse import urljoin
+import logging
 
-def get_urls(htmltext):
-  urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', htmltext)
-  return urls
-
-def get_urls2(htmltext):
+def get_urls(baseurl, htmltext):
   soup = BeautifulSoup(htmltext)
   ret = []
   links = soup.find_all('a')
   for tag in links:
     link = tag.get('href', None)
     if link != None:
-      ret.append(link)
+      ret.append(urljoin(baseurl, link))
   return ret
 
-
 def main():
-  seeds = ["http://www.dmoz.org/", "http://en.wikipedia.org/wiki/Main_Page"]
-  frontier = []
+  logging.basicConfig(filename = 'crawler.log', filemode='w', level=logging.INFO)
+  frontier = ["http://www.dmoz.org/", "http://en.wikipedia.org/wiki/Main_Page"]
 
-  for url in seeds:
+  for url in frontier:
+    logging.info("Requesting " + url)
+    print "Requesting " + url
     r = requests.get(url)
     if(200 <= r.status_code <= 299):
-      print
-      print get_urls2(r.text)
+      page_urls = get_urls(url, r.text)
+      frontier += page_urls
+    else:
+      logging.warning("Request for " + url + " was not in 2xx range.")
 
 if __name__ == '__main__':
   main()
