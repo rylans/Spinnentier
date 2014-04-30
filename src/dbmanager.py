9addc4c8b38a5de36116c202ffb731c88878f942
Rylan <rylans@gmail.com>
2014-04-30 13:54:32 -0700

    Added functions to dbmanager to get some rows from the table for debugging purposes.

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -13,7 +13,12 @@ class dbmanager:
       self.con.commit()
 
   def show_all(self):
-    return self.show_pages() + "\n" + self.show_frontier()
+    string = ""
+    string += self.show_pages() + "\n"
+    string += self.sample_pages() + "\n"
+    string += self.show_frontier() + "\n"
+    string += self.sample_frontier()
+    return string
 
   def show_pages(self):
     rows = [row for row in self.con.cursor().execute('SELECT COUNT(*) FROM pages')]
@@ -25,6 +30,20 @@ class dbmanager:
     rows = [row for row in self.con.cursor().execute('SELECT COUNT(*) FROM frontier')]
     count = rows[0][0]
     string = "FRONTIER (" + str(count) + ")"
+    return string
+
+  def sample_pages(self):
+    rows = [row for row in self.con.cursor().execute('SELECT * FROM pages LIMIT 8')]
+    string = "URL, SIZE\n"
+    for r in rows:
+      string += r[0] + ", " + str(r[1]) + "\n"
+    return string
+
+  def sample_frontier(self):
+    rows = [row for row in self.con.cursor().execute('SELECT * FROM frontier LIMIT 8')]
+    string = "URL, PARENT\n"
+    for r in rows:
+      string += r[0] + ", " + str(r[1]) + "\n"
     return string
 
   def get_visited(self):
